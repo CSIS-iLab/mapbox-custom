cc6da5b768364e79bb61127104c580debb4690fb
Paul Franz <paulkfranz@gmail.com>
2018-10-04 12:01:52 -0400

    wbi data commit

--- carto-terrorism-wbi/js/indicators.js
+++ carto-terrorism-wbi/js/indicators.js
@@ -1,6 +1,24 @@
+function getCountryData () {
+				const inputControls = document.querySelectorAll('#controls input');
+				const values = [];
+
+				inputControls.forEach(input => input.checked ? values.push(input.value): null);
+				return values;
+			}
+
+			function applyFilters () {
+				countryDataFilter.set('in', getCountryData());
+			}
+
+			function registerListeners () {
+				document.querySelectorAll('#controls input').forEach(
+					input => input.addEventListener('click', () => applyFilters())
+				);
+			}
+
 // Layer switcher
 
-const basemap = L.tileLayer('https://api.mapbox.com/styles/v1/ilabmedia/cjk8djf7u3g8l2ro6u9p5wq38/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw', {});
+const basemap = L.tileLayer('https://api.mapbox.com/styles/v1/ilabmedia/cjmqo72pevtii2smvg4ww2r52/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw', {});
 
 const satellite = L.tileLayer('https://api.mapbox.com/styles/v1/ilabmedia/cjkjzuir10v132rq8qqxefi6g/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw', {});
 
@@ -31,18 +49,37 @@ const client = new carto.Client({
 
 // Add WBI basemap
 
-const wbi = new carto.source.Dataset('governance_wbi');
+const countryDataFilter = new carto.filter.Category('govern_rating', { in: getCountryData() });
+
+const wbi = new carto.source.SQL('SELECT * FROM governance_wbi');
+wbi.addFilter(countryDataFilter);
+
 const wbiStyle = new carto.style.CartoCSS(`
 	#layer {
-		polygon-fill: ramp([govern_rating], (#fbe6c5, #f2a28a, #dc7176, #b24b65, #70284a), quantiles);
-	  polygon-opacity: 0.5;
+	  polygon-fill: ramp([govern_rating], (#c9dcda, #98c3c4, #68abb8, #45829b, #2a5674), quantiles);
+	  polygon-opacity: 1;
+	  polygon-comp-op: overlay;
 	}
 	#layer::outline {
 	  line-width: 1;
 	  line-color: #FFFFFF;
 	  line-opacity: 0.5;
 	}
+	#layer::labels {
+	  text-name: [country];
+	  text-face-name: 'Open Sans Regular';
+	  text-size: 13;
+	  text-fill: #ffffff;
+	  text-label-position-tolerance: 0;
+	  text-halo-radius: 1.5;
+	  text-halo-fill: #3c5a72;
+	  text-dy: 0;
+	  text-allow-overlap: false;
+	  text-placement: point;
+	  text-placement-type: dummy;
+	}
 `);
+
 const wbiLayer = new carto.layer.Layer(wbi, wbiStyle);
 
 client.addLayer(wbiLayer);
