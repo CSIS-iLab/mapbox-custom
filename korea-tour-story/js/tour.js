bbac55076559377b413d1ad85d24b057b314e844
leezaweb <leeza.web@gmail.com>
2018-10-29 10:51:15 -0400

    add markers

--- korea-tour-story/js/tour.js
+++ korea-tour-story/js/tour.js
@@ -2,16 +2,81 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw";
 
 var map;
+var radius = 20;
 
 document.addEventListener("DOMContentLoaded", function(event) {
   map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/ilabmedia/cjk40a37x21ha2skptjecv1j0",
-    center: [127.41332, 39.0575],
-    zoom: 12,
+    center: [127.41332, 40],
+    zoom: 7,
     bearing: 0,
-    pitch: 45,
+    pitch: 0,
     scrollZoom: false
+  });
+
+  var framesPerSecond = 15;
+  var initialOpacity = 1;
+  var opacity = initialOpacity;
+  var initialRadius = 8;
+  var radius = initialRadius;
+  var maxRadius = 18;
+  map.on("load", function() {
+    // Add a source and layer displaying a point which will be animated in a circle.
+    map.addSource("point", {
+      type: "geojson",
+      data: pointOnCircle(0)
+    });
+
+    map.addLayer({
+      id: "point",
+      source: "point",
+      type: "circle",
+      paint: {
+        "circle-color": "#ff0",
+        "circle-radius": initialRadius,
+        "circle-radius-transition": { duration: 0 },
+        "circle-opacity-transition": { duration: 0 }
+      }
+    });
+
+    map.addLayer({
+      id: "point1",
+      source: "point",
+      type: "circle",
+      paint: {
+        "circle-radius": initialRadius,
+        "circle-color": "#ff0"
+      }
+    });
+
+    function animateMarker(timestamp) {
+      setTimeout(function() {
+        requestAnimationFrame(animateMarker);
+
+        radius += (maxRadius - radius) / framesPerSecond;
+        opacity -= 0.9 / framesPerSecond;
+        opacity = Math.max(0, opacity);
+
+        map.setPaintProperty("point", "circle-radius", radius);
+        map.setPaintProperty("point", "circle-opacity", opacity);
+
+        if (opacity <= 0) {
+          radius = initialRadius;
+          opacity = initialOpacity;
+        }
+      }, 1000 / framesPerSecond);
+
+      // Update the data to a new position based on the animation timestamp. The
+      // divisor in the expression `timestamp / 1000` controls the animation speed.
+      map.getSource("point").setData(pointOnCircle(timestamp / 1000));
+
+      // Request the next frame of the animation.
+      // requestAnimationFrame(animateMarker);
+    }
+
+    // Start the animation.
+    animateMarker(0);
   });
 });
 
@@ -67,6 +132,10 @@ window.onscroll = function() {
     if (isElementOnScreen(chapterName)) {
       setActiveChapter(chapterName);
       break;
+    } else {
+      let activeElement = document.getElementById(activeChapterName);
+
+      if (activeElement) activeElement.setAttribute("class", "");
     }
   }
 };
@@ -74,18 +143,26 @@ window.onscroll = function() {
 var activeChapterName = "Sino-Korean";
 
 function setActiveChapter(chapterName) {
-  if (chapterName === activeChapterName) return;
+  // if (chapterName === activeChapterName) return;
 
   map.flyTo(chapters[chapterName]);
 
   document.getElementById(chapterName).setAttribute("class", "active");
-  document.getElementById(activeChapterName).setAttribute("class", "");
+  // if (activeChapterName)
+  //   document.getElementById(activeChapterName).setAttribute("class", "");
 
   activeChapterName = chapterName;
 }
 
 function isElementOnScreen(id) {
-  var element = document.getElementById(id);
+  var element = document.getElementById(id).parentElement;
   var bounds = element.getBoundingClientRect();
-  return bounds.top < window.innerHeight && bounds.bottom > 0;
+  return bounds.top - 20 < window.innerHeight && bounds.bottom > 0;
+}
+
+function pointOnCircle(angle) {
+  return {
+    type: "Point",
+    coordinates: chapters[activeChapterName].center
+  };
 }
