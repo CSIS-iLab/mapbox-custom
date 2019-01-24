378b982fd89c3fcff4218776cf7e613ec0486e55
leezaweb <leeza.web@gmail.com>
2019-01-24 14:48:24 -0500

    add "of Venezuela"

--- americas-venezeula-presidential-recognition/js/app.js
+++ americas-venezeula-presidential-recognition/js/app.js
@@ -73,13 +73,13 @@ recognitionLayer.on(carto.layer.events.FEATURE_OVER, function(e) {
             e.data.country
           }</div><div class='popupEntryStyle'>${
             e.data.country
-          } recognizes Nicolás Maduro as President</div>`
+          } recognizes Nicolás Maduro as President of Venezuela</div>`
         : e.data.guaido.toLowerCase() === "n"
           ? `<div class='popupHeaderStyle'>${
               e.data.country
             }</div><div class='popupEntryStyle'>${
               e.data.country
-            } recognizes Juan Guaidó as President</div>`
+            } recognizes Juan Guaidó as President of Venezuela</div>`
           : `<div class='popupHeaderStyle'>${
               e.data.country
             }</div><div class='popupEntryStyle'>unconfirmed</div>`;
