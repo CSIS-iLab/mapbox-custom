1ee61a06b907cd6c40d23ff039c9437d3fb8f74d
Paul Franz <paulkfranz@gmail.com>
2019-05-20 19:22:04 -0400

    added table id for sql query

--- americas-venezeula-presidential-recognition/js/app.js
+++ americas-venezeula-presidential-recognition/js/app.js
@@ -24,7 +24,7 @@ var client = new carto.Client({
   username: "csis"
 });
 
-var recognitionSQL = new carto.source.SQL("SELECT * FROM ");
+var recognitionSQL = new carto.source.SQL("SELECT * FROM guaido_recognition");
 
 var baseStyle =
   '\n[guaido = \'?\']{\n  comp-op: multiply;\n  polygon-pattern-file: url(https://csis-ilab.github.io/mapbox-custom/americas-venezeula-presidential-recognition/images/stripe.png);\n  }\n\n  ::outline {\n    line-color: #fff;\n    line-width: 1;\n    polygon-fill: ramp([guaido],(#f4da97,#5cc181,transparent),("Y","N"),"=");\n\n    }\n  #layer::labels {\n    text-name: [country];\n    text-face-name: \'Open Sans Bold\';\n    text-transform: uppercase;\n    text-size: 12;\n    text-fill: #fff;\n    text-label-position-tolerance: 0;\n    text-halo-radius: 1.25;\n    text-halo-fill: #504e4e;\n    text-dy: 0;\n    text-opacity: ramp([guaido],(1,1,0),("Y","N"),"=");\n    text-halo-opacity: ramp([guaido],(1,1,0),("Y","N"),"=");\n    text-allow-overlap: false;\n    text-placement: point;\n    text-placement-type: dummy;\n  }\n';
