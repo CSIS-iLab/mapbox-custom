275391728048b25711e2dca905b20014455f9755
leezaweb <leeza.web@gmail.com>
2019-05-21 15:35:04 -0400

    new colors

--- africa-ports/africaPorts.js
+++ africa-ports/africaPorts.js
@@ -1,22 +1,22 @@
-(async function() {
+;(async function() {
   var map = await makeMap({
-    googleSheet: "11hN6uzXcO7amn5bT-a09T8oEvgrw7lZ-oBfGtLRzpws",
-    mapID: "africa",
+    googleSheet: '11hN6uzXcO7amn5bT-a09T8oEvgrw7lZ-oBfGtLRzpws',
+    mapID: 'africa',
     formatPopupContent: function(feature, map) {
       return (
         '<div class="popupTitleStyle">' +
         feature.properties.port +
         '</div><div class="popupEntryStyle">' +
         feature.properties.port_city +
-        ", " +
+        ', ' +
         feature.properties.port_country +
         '</div><div class="popupHeaderStyle">Investment Type</div><ul class="popupEntryStyle">' +
         feature.properties.investment_type
-          .split(",")
+          .split(',')
           .map(r => `<li>${capitalize(r)}</li>`)
-          .join("") +
-        "</ul>"
-      );
+          .join('') +
+        '</ul>'
+      )
     }
-  });
-})();
+  })
+})()
