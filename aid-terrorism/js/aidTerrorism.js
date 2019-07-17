1e06e777808c0b2d4a300fea08fb9f5848d8405a
leezaweb <leeza.web@gmail.com>
2019-07-17 10:36:37 -0400

    split palestinian territories out

--- aid-terrorism/js/aidTerrorism.js
+++ aid-terrorism/js/aidTerrorism.js
@@ -14,21 +14,54 @@
           return f.properties
         })
 
-      var countryData = jsons
-        .filter(function(f) {
-          return f.country === feature.properties.country
-        })
-        .reduce(function(a, b) {
-          return {
-            terrorism: a.terrorism ? a.terrorism : b.terrorism,
-            foreign_assistance: a.foreign_assistance
-              ? a.foreign_assistance
-              : b.foreign_assistance,
-            actual_assistance: a.actual_assistance
-              ? a.actual_assistance
-              : b.actual_assistance
-          }
-        })
+      var palestinianTerritories = ['Gaza Strip', 'West Bank', 'Palestine']
+      var countryData0 = jsons.filter(function(f) {
+        return palestinianTerritories.indexOf(feature.properties.country) > -1
+          ? palestinianTerritories.indexOf(f.country) > -1
+          : f.country === feature.properties.country
+      })
+
+      var countryData = countryData0.reduce(function(a, b) {
+        var countryTerrorismData = ''
+
+        switch (feature.properties.country) {
+          case 'West Bank':
+            countryTerrorismData +=
+              a.country === 'West Bank' && a.terrorism
+                ? a.terrorism
+                : b.country === 'West Bank' && b.terrorism
+                  ? b.terrorism
+                  : ''
+            break
+
+          case 'Gaza Strip':
+            countryTerrorismData +=
+              a.country === 'Gaza Strip' && a.terrorism
+                ? a.terrorism
+                : b.country === 'Gaza Strip' && b.terrorism
+                  ? b.terrorism
+                  : ''
+            break
+
+          case 'Palestine':
+            countryTerrorismData += a.terrorism + '~' + b.terrorism
+            break
+
+          default:
+            countryTerrorismData = a.terrorism ? a.terrorism : b.terrorism
+        }
+
+        return {
+          country: feature.properties.country,
+          terrorism: countryTerrorismData,
+          foreign_assistance: a.foreign_assistance
+            ? a.foreign_assistance
+            : b.foreign_assistance,
+          actual_assistance: a.actual_assistance
+            ? a.actual_assistance
+            : b.actual_assistance
+        }
+      })
 
       var groups = '',
         assistance = '',
@@ -38,6 +71,9 @@
         groups = `<br><div class="popupHeaderStyle">Terrorist Groups</div>
         <ul>${terrorism
           .split('~')
+          .filter(function(t) {
+            return t
+          })
           .map(function(group) {
             return `<li class='popupEntryStyle'>${group}</li>`
           })
@@ -48,7 +84,7 @@
         assistance = `<div class="popupHeaderStyle">Foreign Assistance: $${countryData.actual_assistance.toLocaleString()}</div>`
       }
 
-      return `<div class="popupTitleStyle">${feature.properties.country}</div>
+      return `<div class="popupTitleStyle">${countryData.country}</div>
         ${assistance}      ${groups}`
     }
   })
